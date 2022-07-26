5151fc37a62ba6734a91569b286d43a961500a0b
PedroVin <92653786+pedrovsilva00@users.noreply.github.com>
2022-07-26 20:14:26 -0300

    Update codedb.py

--- codedb.py
+++ codedb.py
@@ -2,7 +2,46 @@
 import sqlite3 as db
 import hashlib
 import keyring
+import time
+import getpass 
+import random
+import string
+import time
+from progressbar import Bar, AdaptiveETA, Percentage, ProgressBar,  RotatingMarker, FileTransferSpeed
+import cryptocode
 
+
+def logo():
+    print ('     ___________________________________________________________ ')
+    print ('    |      _______   _______   _______   _______   _______      |')
+    print ('    |     (  ____ ) (  ___  ) (  ____ \ (  ____ \ (  ___  )     |')
+    print ('    |     | (    )| | (   ) | | (    \/ | (    \/ | (   ) |     |')
+    print ('    |     | (____)| | (___) | | (_____  | (_____  | (___) |     |')
+    print ('    |     |  _____) |  ___  | (_____  ) (_____  ) |  ___  |     |')
+    print ('    |     | (       | (   ) |       ) |       ) | | (   ) |     |')
+    print ('    |     | )       | )   ( | /\____) | /\____) | | )   ( |     |')
+    print ('    |     |/        |/     \| \_______) \_______) |/     \|     |')
+    print ('    |      _______   _______   _                   _______      |')
+    print ('    |     (  ____ \ (  ____ \ ( (    /| |\     /| (  ___  )     |')
+    print ('    |     | (    \/ | (    \/ |  \  ( | | )   ( | | (   ) |     |')
+    print ('    |     | (_____  | (__     |   \ | | | (___) | | (___) |     |')
+    print ('    |     (_____  ) |  __)    | (\ \) | |  ___  | |  ___  |     |')
+    print ('    |           ) | | (       | | \   | | (   ) | | (   ) |     |')
+    print ('    |     /\____) | | (____/\ | )  \  | | )   ( | | )   ( |     |')
+    print ('    |     \_______) (_______/ |/    )_) |/     \| |/     \|     |')
+    print ('    |___________________________________________________________|')
+    time.sleep(2)
+    print('\x1b[2J')
+
+
+def barra():
+    widgets = ['Carregando: ', Percentage(), ' ', Bar(marker=RotatingMarker()),
+               ' ', AdaptiveETA(), ' ', FileTransferSpeed(unit='J')]
+    pbar = ProgressBar(widgets=widgets, maxval=10000000).start()
+    for i in range(100000):
+        # do something
+        pbar.update(100*i+1)
+    pbar.finish()
 
 class connection(): #conexão banco dados
     def __init__(self):
@@ -10,6 +49,7 @@ class connection(): #conexão banco dados
             self.conn = db.connect('passbank.db')
             self.cur = self.conn.cursor()
             print('Conexão bem sucedida')
+            
         except Exception as e:
             print("Erro na conexão", e)
 
@@ -41,87 +81,134 @@ class connection(): #conexão banco dados
         self.cur.execute(sql, params or ())
         return self.fetchall()
 
+def cripto(s1, s2):
+    return "".join([chr(ord(c1) ^ ord(c2)) for (c1, c2) in zip(s1,s2)])
+
 class login(connection):
     def __init__(self):
         connection.__init__(self)
-    def insert(self,botid,botuser,botsen,botgru): # Adicionar novos dados
+    def insert(self,btu,btc,btg,bte,k): # Adicionar novos dados
         try:
+            btu_cry = cripto(btu,k)
+            btc_cry = cripto(btc,k)
+            btg_cry = cripto(btg,k)
+            bte_cry = cripto(bte,k)
             self.cur.execute(f"""
-                    INSERT INTO login (id, usuario, senha, grupo) VALUES (?, ?, ?, ?)
-                   ;""", (botid,botuser,botsen,botgru))
+                    INSERT INTO aevr (uiop, cvbn, ghjk, erty) VALUES (?, ?, ?, ?)
+                   ;""", [btu_cry,btc_cry,btg_cry,bte_cry])
             self.commit()
+            barra()
+            print('Banco de Dados atualizado')
         except Exception as e:
             print("Erro ",e, " ao atualizar o banco de dados")
 
-    def delete(self,idele): # Deletar dados / procura pelo id
+    def delete(self,delu,k): # Deletar dados / procura pelo id
         try:
-            self.cur.execute(f"""DELETE FROM login WHERE id = '{idele}'""")  
+            delu_cry = cripto(delu,k)
+            self.cur.execute(f"""DELETE FROM aevr WHERE uiop = '{delu_cry}'""")  
             self.commit()  
-            print('Conta: ',idele,' deletada')
+            print('Conta: ',delu,' deletada')
         except Exception as e:
             print("Erro ",e, " ao deletar no banco de dados")  
 
-    def updateusu(self,upid, new_usu): # Editar usuario 
+    def updatecv(self,upu, new_c,k): # Editar usuario 
         try:
-            self.execute(f"UPDATE login SET usuario = '{new_usu}' WHERE id= '{upid}'")  
+            upu_cry = cripto(upu,k)
+            new_cry = cripto(new_c,k)
+            self.execute(f"UPDATE aevr SET cvbn = '{new_cry}' WHERE uiop= '{upu_cry}'")  
             self.commit()
-            print("Usuario alterado do id: ",upid)
+            print("Usuario alterado da conta: ",upu)
         except Exception as e:
             print("Erro ",e, " ao atualizar o usuario")
 
-    def updatesen(self,upid, new_sen): # Editar senha
+    def updategh(self,upu, new_g,k): # Editar senha
         try:
-            self.execute(f"UPDATE login SET senha = '{new_sen}' WHERE id= '{upid}'")  
+            upu_cry = cripto(upu,k)
+            new_cry = cripto(new_g,k)
+            self.execute(f"UPDATE aevr SET ghjk = '{new_cry}' WHERE uiop= '{upu_cry}'")  
             self.commit()
-            print("Senha alterada do id: ",upid)
+            print("Senha alterada da conta: ",upu)
         except Exception as e:
             print("Erro ",e, " ao atualizar senha")
 
-    def updategru(self,upid, new_gru): # Editar grupo
+    def updateer(self,upu, new_e,k): # Editar grupo
         try:
-            self.execute(f"UPDATE login SET grupo = '{new_gru}' WHERE id= '{upid}'")  
+            upu_cry = cripto(upu,k)
+            new_cry = cripto(new_e,k)
+            self.execute(f"UPDATE aevr SET erty = '{new_cry}' WHERE uiop= '{upu_cry}'")  
             self.commit()
-            print("Grupo alterado do id: ",upid)
+            print("Grupo alterado da conta: ",upu)
         except Exception as e:
             print("Erro ",e, " ao atualizar o grupo")
 
-    def procuraid(self,proid): # Pesquisa pelo id 
-        apps = (self.query(f"""
-                    SELECT usuario, senha,grupo FROM login WHERE id = '{proid}'
-                  ;"""))
-        if apps: 
-            print (' Resultado da procura pelo id: ',proid)
-            print('\n')
+    def procuraui(self,prou,k): # Pesquisa pelo id
+        if prou == 'all':
+            apps = self.query("SELECT uiop FROM aevr")
+            print('\nTodas Contas: \n')
+            x =''
             for row in apps:
-                print('USUARIO --> ',row[0])
-                print(' SENHA  --> ',row[1])
-                print(' GRUPO  --> ',row[2])
+                um = cripto(row[0],k)
+                print(' ID  --> ',um)
                 print('\n')
-        else: print('O id ', proid, ' não foi encontrado') 
+        else:
+            pu = cripto(prou,k)
+            apps = (self.query(f"""
+                    SELECT cvbn, ghjk ,erty FROM aevr WHERE uiop = '{pu}'
+                  ;"""))
+            if apps: 
+                print (' Resultado da procura pela conta: ',prou)
+                print('\n')
+                for row in apps:
+                    um = cripto(row[0],k) 
+                    dos = cripto(row[1],k)
+                    tre = cripto(row[2],k)
+                    print('USUARIO --> ',um)
+                    print(' SENHA  --> ',dos)
+                    print(' GRUPO  --> ',tre)
+                    print('\n')
+            else: x = ('O id ', prou, ' não foi encontrado')
+        return print (x) 
 
-    def procuragru(self,progru): # Pesquisa por grupo
-        apps = (self.query(f"""
-                    SELECT id, usuario, senha FROM login WHERE grupo = '{progru}'
-                  ;"""))
-        if apps: 
-            print (' Resultado da procura pelo grupo: ',progru)
-            print('\n')
+    def procuraer(self,proe,k): # Pesquisa por grupo
+        if proe == 'all':
+            apps = self.query("SELECT erty FROM aevr")
+            print('\nTodos os grupos: \n')
+            x =''
             for row in apps:
-                print('  ID    --> ',row[0])
-                print('USUARIO --> ',row[1])
-                print(' SENHA  --> ',row[2])
+                um = cripto(row[0],k)
+                print(' GRUPO  --> ',um)
                 print('\n')
-        else: print('O grupo ', progru, ' não foi encontrado') 
+        else:
+            pe = cripto(proe,k)
+            apps = (self.query(f"""
+                    SELECT uiop, cvbn, ghjk FROM aevr WHERE erty = '{pe}'
+                  ;"""))
+            if apps: 
+                print (' Resultado da procura pelo grupo: ',proe)
+                print('\n')
+                for row in apps:
+                    um = cripto(row[0],k) 
+                    dos = cripto(row[1],k)
+                    tre = cripto(row[2],k)
+                    print('  ID    --> ',um)
+                    print('USUARIO --> ',dos)
+                    print(' SENHA  --> ',tre)
+                    print('\n')
+            else:  x = ('O grupo ', proe ,' não foi encontrado') 
+        return print (x)
 
     def main_menu(): # menu principal
         print('------------------------------')
         print('1- Inserir uma nova conta')
-        print('2- Vizualizar as contas salvas')
-        print('3- Pesquisar uma conta')
+        print('2- Procurar conta por id')
+        print('3- Procurar conta por grupo')
         print('4- Excluir uma conta')
         print('5- Editar uma conta')
+        print('6- Gerador de senhas')
         print('0- Sair')
-        print('------------------------------')
+        print('------------------------------\n')
+        z =input('Qual opção desejada:  ')
+        return z
 
     def menuzin(): #menu de edição
         print('------------------------------')
@@ -130,140 +217,147 @@ class login(connection):
         print('3- Mudar o grupo')
         print('4- Mudar usuario e senha')
         print('------------------------------\n')
+        z =input('Sua escolha: ')
+        return z
+
 
     def continua(): # Continuar ou não a usar o banco de dados
-        cc = input(' Deseja continuar? (S) (N)')
-        if cc == 'S' or cc == 's':
+        cc = input(' Deseja continuar? (S) (N) ')
+        if cc == 'S' or cc == 's' or cc == '1':
             vano = True
-        elif cc=='N' or cc=='n':
+        elif cc=='N' or cc=='n' or cc == '0':
             vano =False
         return vano
         
     def clear_terminal():
         return print('\x1b[2J')
+    
+    def gerador (x):
+        xxx = 0
+        senha = ''
+        for xxx in range(x):
+            senha += random.choice((string.ascii_letters) + (string.digits) + "#$%&*^?@_!-ç=]}{[")
+        return senha
+
 
 
 class autoriza(connection): # Intereção com o banco de dados
     def __init__(self):
-            print('Bem-vindo Deus Grego')
+            logo()
                 # Cadastro do admin pelo hash sha512 - padrão admin
             self.admin = '58b5444cf1b6253a4317fe12daff411a78bda0a95279b1d5768ebf5ca60829e78da944e8a9160a0b6d428cb213e813525a72650dac67b88879394ff624da482f'
                 # Cadastro da senha pelo hash sha512 - padrão admin
             self.master = '58b5444cf1b6253a4317fe12daff411a78bda0a95279b1d5768ebf5ca60829e78da944e8a9160a0b6d428cb213e813525a72650dac67b88879394ff624da482f' 
             keyring.set_password('master', self.admin, self.master) # Armazenamento de credenciais pela biblioteca keyring
-            pede_admin = input(' Admin: ')
-            pede_master = input ('Senha Mestra: ')
+            pede_admin = getpass.getpass('User: ')
+            pede_master = getpass.getpass ('Password: ')
                 #Verificação em duas etapas - padrão 1
-            salt = input('Verificação 2 etapas: ')
+            salt = getpass.getpass('Lucky Number: ')
             cc = pede_admin + salt
             mm = pede_master + salt
             hm = hashlib.sha512(mm.encode())
-            hashs = hm.hexdigest() # transformação do admin fornecido em hash sha512
+            hashs = hm.hexdigest() # transformação do senha fornecido em hash sha512
             hc = hashlib.sha512(cc.encode())
-            hashc = hc.hexdigest() # transformação da senha fornecida em hash sha512
+            hashc = hc.hexdigest() # transformação da admin fornecida em hash sha512
             aa = keyring.get_password("master",hashc)
+            barra()
 
             if (aa == hashs): # Verificação de autenticidade
                 connection.__init__(self)
                 cont = True
                 while cont == True:
-                    login.main_menu()
-                    xxx = input('Qual opção desejada:  ')
+                    xxx = login.main_menu()
                     if xxx == '1': # ADD nova linha
                         login.clear_terminal()
                         print('Dados do novo cadastro')
-                        botid_new = input('ID:  ')
-                        botuser_new = input('Usuario:  ')
-                        botsen_new = input('Senha:  ')
-                        botgru_new = input('Grupo:  ')
-                        login.insert(self,botid_new,botuser_new,botsen_new,botgru_new)
-                        print('Banco de Dados atualizado')
+                        pdu = input('ID:  ')
+                        pdc = input('Usuario:  ')
+                        pdg = input('Senha:  ')
+                        pde = input('Grupo:  ')
+                        login.insert(self,pdu,pdc,pdg,pde,aa)
                         print('\n')
                         cont = (login.continua())
 
-                    elif xxx == '2': # Mostrar todo o banco de dados
+                    elif xxx == '2': # Procurar por id
                         login.clear_terminal()
-                        apps = self.query("SELECT * FROM login")
-                        for row in apps:
-                            print('  ID    --> ',row[0])
-                            print('USUARIO --> ',row[1])
-                            print(' SENHA  --> ',row[2])
-                            print(' GRUPO  --> ',row[3])
-                            print('\n')
+                        seru = input('Qual id que procuras? ')
+                        login.clear_terminal()
+                        barra()
+                        login.procuraui(self,seru,aa)
                         cont = (login.continua())
 
-                    elif xxx== '3': #Pesquisar dentro do banco de dados
+                    elif xxx== '3':  # procura pelo grupo
                         login.clear_terminal()
-                        tipo = input('Pesquisa por id ou grupo?  ')
-                        if tipo == 'id': # procura por id
-                            ser_id = input('Qual id que procuras? ')
-                            login.clear_terminal()
-                            login.procuraid(self,ser_id)
-                        else: # procura pelo grupo
-                            ser_gru = input('Qual grupo que procuras? ')
-                            login.clear_terminal()
-                            login.procuragru(self,ser_gru)
+                        sere = input('Qual grupo que procuras? ')
+                        login.clear_terminal()
+                        barra()
+                        login.procuraer(self,sere,aa)
+                        cont = (login.continua())
 
                     elif xxx=='4': #excluir dados do banco
                         login.clear_terminal()
-                        idsai = input('Qual conta deseja excluir: ')
-                        login.delete(self,idsai)
+                        delui = input('Qual conta deseja excluir: ')
+                        login.delete(self,delui,aa)
                         print('\n')
                         cont = (login.continua())
 
                     elif xxx=='5': #Editar conta
                         login.clear_terminal()
-                        idedi = input('Qual ID da conta deseja mudar? ')
+                        muu = input('Qual ID da conta deseja mudar? ')
                         print('\n')
-                        login.procuraid(self,idedi)
-                        login.menuzin()
-                        temp = input('Sua escolha: ')
+                        login.procuraui(self,muu,aa)
+                        temp = login.menuzin()
                         if temp == '1': # Alterar somente usuario
                             login.clear_terminal()
-                            new_u = input(' Qual o novo usuario? ')
-                            login.updateusu(self,idedi,new_u)
-                            login.procuraid(self,idedi)
+                            new_c = input(' Qual o novo usuario? ')
+                            login.updatecv(self,muu,new_c,aa)
+                            barra()
+                            login.procuraui(self,muu,aa)
                             print('\n')
                             cont = (login.continua())
                         elif temp =='2': # Alterar somente senha
                             login.clear_terminal()
-                            new_s = input(' Qual a nova senha? ')
-                            login.updatesen(self,idedi,new_s)
-                            login.procuraid(self,idedi)
+                            new_g = input(' Qual a nova senha? ')
+                            login.updategh(self,muu,new_g,aa)
+                            barra()
+                            login.procuraui(self,muu,aa)
                             print('\n')
                             cont = (login.continua())
                         elif temp =='3': # Alterar somente grupo
                             login.clear_terminal()
                             new_g = input(' Qual o novo grupo? ')
-                            login.updategru(self,idedi,new_g)
-                            login.procuraid(self,idedi)
+                            login.updateer(self,muu,new_g,aa)
+                            barra()
+                            login.procuraui(self,muu,aa)
                             print('\n')
                             cont = (login.continua())
                         elif temp =='4': # Alterar usuario e senha
                             login.clear_terminal()
-                            new_u = input(' Qual o novo usuario? ')
-                            login.updateusu(self,idedi,new_u)
-                            new_s = input(' Qual a nova senha? ')
-                            login.updatesen(self,idedi,new_s)
-                            login.procuraid(self,idedi)
+                            new_c = input(' Qual o novo usuario? ')
+                            login.updatecv(self,muu,new_c,aa)
+                            new_g = input(' Qual a nova senha? ')
+                            login.updategh(self,muu,new_g,aa)
+                            barra()
+                            login.procuraui(self,muu,aa)
                             print('\n')
                             cont = (login.continua())
                         else: print('Digita serto parça')
+                    elif xxx == '6':
+                        login.clear_terminal()
+                        print ('Gerador de Senhas\n')
+                        temp = int(input('Tamanho desejado: \n'))
+                        print (login.gerador(temp))
+                        cont = login.continua()
+
                     elif xxx == '0':
                         cont = False
                 connection.__exit__
-                print(' Bye bye bitch ')
+                print(' Bye ')
+                time.sleep(1)
             else: 
                 connection.__exit__
                 print(' Tu não conhece meus segredos')
-
+                time.sleep(1)
         
 if __name__ == "__main__":
     banco = autoriza()
-
-
-
-        
-        
-
-            
